e9c525ca42a39061786658bccea162b7700f44c9
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-11-10 23:29:14 -0800

    Handle command line with arguments

---
+++ Simple_shell_0.2/my_printf.c
@@ -0,0 +1,10 @@
+#include "shell.h"
+/**
+ * my_printf - function that print to standard output
+ * @commands: pointer to array of string
+ * Return: nothing
+ */
+void my_printf(const char *commands)
+{
+	write(STDOUT_FILENO, commands, strlen(commands));
+}
