4e693b1b3b58c21a99351c114a56adf611a19f56
pheyishayo <amzatsayi@yahoo.com>
2023-11-14 22:33:25 -0800

    a UNIX command line interpreter

---
+++ Simple_shell_0.1+/_shell.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+/**
+ * main - Entry point
+ * @ac: this count the number of arguments
+ * @av: this store number of arguments passed to
+ * the command line. it a pointer to array of
+ * string.
+ * Return: 0 on success
+ */
+
+int main(int ac, char **av)
+{
+	char command_line[150];
+	(void)ac;
+
+	while (1)
+	{
+		_prompt();
+		user_input(command_line, sizeof(command_line));
+		if (strspn(command_line, " ") < strlen(command_line))
+		{
+			arg_line(command_line, av);
+		}
+	}
+	return (0);
+}
---
+++ Simple_shell_0.1+/execution2.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+/**
+ * arg_line - This function takes arguments
+ * @input: pointer to constant string of character
+ * @av: pointer to array of characters
+ */
+void arg_line(const char *input, char **av __attribute__((unused)))
+{
+	const int max = 1024;
+	pid_t child_pid;
+	char *token, *delim = "\n ";
+	int i, j, status;
+	char **args = malloc(sizeof(char *) * max);
+
+	if (args == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("fork");
+		free(args);
+		exit(EXIT_FAILURE);
+	}
+	if (child_pid == 0)
+	{
+		for (i = 0; i < max; i++)
+			args[i] = NULL;
+		token = strtok((char *)input, delim);
+		for (j = 0; token != NULL && (args[j] = token); j++)
+			token = strtok(NULL, delim);
+		if (args[0] != NULL)
+		{
+			if (execve(args[0], args, NULL) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		free(args);
+		exit(EXIT_SUCCESS);
+	}
+	else
+		waitpid(child_pid, &status, 0);
+}
